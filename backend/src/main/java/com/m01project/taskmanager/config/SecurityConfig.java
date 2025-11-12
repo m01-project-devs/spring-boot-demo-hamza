cc2ada032802c80031da09e871b43949a5da9600
Sakarya,Hamza Ali <asakarya@rbbn.com>
2025-11-12 21:14:46 +0100

    DEV-12-revert-csrf-back

--- backend/src/main/java/com/m01project/taskmanager/config/SecurityConfig.java
+++ backend/src/main/java/com/m01project/taskmanager/config/SecurityConfig.java
@@ -11,7 +11,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // close CSRF
                 // allow all requests.
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
